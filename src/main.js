0471a4d2f74921bad58d733b620bade4be162652
wulzt <3217005157@mail2.gdut.edu.cn>
2018-08-08 20:37:30 +0800

    20:37

--- src/main.js
+++ src/main.js
@@ -1,19 +1,7 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import router from './router'
 
 import App from './App.vue'
-import Home from './components/Home.vue'
-
-Vue.use(VueRouter)
-const router = new VueRouter({
-  routes: [
-    {
-      path: './',
-      name: 'Home',
-      component: Home
-    },
-  ]
-})
 
 new Vue({
   el: '#app',
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
-// import Vue from 'vue'
-// import Router from 'vue-router'
-// import Home from '../components/Home'
-//
-// Vue.use(Router)
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '../components/Home'
 
-// export default {
-//   routes: [
-//     {
-//       path: './',
-//       name: 'Home',
-//       component: Home
-//     },
-//   ]
-// }
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+  ]
+})
